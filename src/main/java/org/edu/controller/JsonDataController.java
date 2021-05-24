eabba5b0fc6e4857df8689331cc9de4f3dcd2c60
kimjina <927103@naver.com>
2021-05-24 20:26:13 +0900

    헤로쿠 베포

--- src/main/java/org/edu/controller/JsonDataController.java
+++ src/main/java/org/edu/controller/JsonDataController.java
@@ -12,6 +12,7 @@ import org.jboss.logging.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,6 +23,19 @@ public class JsonDataController {
 	@Inject
 	private IF_MemberDAO memberDAO;
 	private Logger logger = Logger.getLogger(SimpleLog.class);
+	
+	//RestAPI서버 : 안드로이드앱에서 회원목록중 선택한 id 삭제.
+	@RequestMapping(value="/android/delete/{user_id}", method=RequestMethod.POST)
+	public ResponseEntity<String> androidDelete(@PathVariable("user_id") String user_id) {
+		ResponseEntity<String> entity = null;
+		try {
+			memberDAO.deleteMember(user_id);
+			entity = new ResponseEntity<>("success", HttpStatus.OK);//200값을 success텍스트와 함께 앱으로 전송
+		} catch (Exception e) {
+			entity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);//400을 앱으로 전송
+		}
+		return entity;//텍스트 데이터를 리턴
+	}
 	
 	//RestAPI인증서버 : 안드로이드앱에서 로그인에 사용됨(아래)
 	@RequestMapping(value="/android/login", method=RequestMethod.POST)
@@ -37,10 +51,10 @@ public class JsonDataController {
 				entity = new ResponseEntity<>(memberVO, HttpStatus.OK);//200
 			} else {
 				logger.debug("계정정보 불일치");
-				entity = new ResponseEntity<>(HttpStatus.NO_CONTENT);
+				entity = new ResponseEntity<>(HttpStatus.NO_CONTENT);//204
 			}
 		} catch (Exception e) {
-			entity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+			entity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);//400
 		}
 		return entity;//json 데이터(Key:Value,)로 반환값 리턴
 	}
---
+++ src/test/java/api.java
@@ -0,0 +1,30 @@
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+
+//오리지날 자바프로그램 형식: 자바이론공부 -> 스프링 웹프로그램과는 형식이 틀립니다.
+public class api {
+
+	private String getLocalServerIp()
+	{
+		try
+		{
+		    for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();)
+		    {
+		        NetworkInterface intf = en.nextElement();
+		        for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();)
+		        {
+		            InetAddress inetAddress = enumIpAddr.nextElement();
+		            if (!inetAddress.isLoopbackAddress() && !inetAddress.isLinkLocalAddress() && inetAddress.isSiteLocalAddress())
+		            {
+		            	return inetAddress.getHostAddress().toString();
+		            }
+		        }
+		    }
+		}
+		catch (SocketException ex) {}
+		return null;
+	}
+}
